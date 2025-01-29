ef9c0d10be552972327ad997e8ea6ed27e4f75f8
Bonato12 <sebabonato12@gmail.com>
2025-01-29 20:19:26 -0300

    Agregue metodo nuevo

--- generate_dataset.py
+++ generate_dataset.py
@@ -7,16 +7,20 @@ def generate_dataset_muestra():
     input = cfg.DATASET_ES_PATH
     dataset  = import_dataset(input) 
     dataset = dataset[['label', 'tweet']]
-    dataset = dataset[dataset['label'].notnull()]  # Elimina valores nulos
-    dataset['id'] = np.random.randint(10000000, 20000000, size=len(dataset))  # Genera números aleatorios entre 1 y 100
+    dataset = dataset[dataset['label'].notnull()] 
+    dataset['id'] = np.random.randint(10000000, 20000000, size=len(dataset))
     dataset = dataset[['id','label', 'tweet']]
-    dataset['label'] = dataset['label'].apply(lambda x: int(x))  # Convertir todo explícitamente a entero
+    dataset['label'] = dataset['label'].apply(lambda x: int(x)) 
     print(dataset['label'].dtype)
     export_dataset(dataset,cfg.DATASET_ES_MUESTRA_PATH)
     print("Fin")
 
 def get_dataset():
     train_dataset  = import_dataset(cfg.DATASET_ES_PATH) 
+    return train_dataset
+
+def get_dataset_muestra():
+    train_dataset  = import_dataset(cfg.DATASET_ES_MUESTRA_PATH) 
     return train_dataset
 
 def get_dataset_preprocessed():
