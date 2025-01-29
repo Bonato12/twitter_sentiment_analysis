5116de9fbe7802de9a6e6f1063bc02bad29cd992
Bonato12 <sebabonato12@gmail.com>
2025-01-29 20:21:52 -0300

    Mejora en preprocessing_dataset

--- preprocessing_dataset.py
+++ preprocessing_dataset.py
@@ -1,25 +1,13 @@
-from util import import_dataset,export_dataset
+from util import export_dataset
 from preprocessing_text import normalize
 import config as cfg
+from generate_dataset import get_dataset_muestra
 
 def preprocessing_dataset():
-    input = cfg.DATASET_TRAIN_MUESTRA_PATH_TEST
-    dataset  = import_dataset(input) 
-    dataset['clean_tweet'] = dataset['tweet'].apply(normalize)
-    dataset = dataset[dataset['clean_tweet'].notnull()]  # Elimina valores nulos
-    dataset = dataset[dataset['clean_tweet'].str.strip() != '']  # Elimina cadenas vacías
-    export_dataset(dataset,cfg.DATASET_TRAIN_MUESTRA_PREPROCESSED_PATH)
-    print("Fin")
-
-def preprocessing_dataset_es():
-    input = cfg.DATASET_ES_MUESTRA_PATH
-    dataset  = import_dataset(input) 
+    dataset  = get_dataset_muestra() 
     dataset['clean_tweet'] = dataset['tweet'].apply(normalize)
     #dataset['clean_tweet'] = dataset['tweet']
     dataset = dataset[dataset['clean_tweet'].notnull()]  # Elimina valores nulos
     dataset = dataset[dataset['clean_tweet'].str.strip() != '']  # Elimina cadenas vacías
     export_dataset(dataset,cfg.DATASET_ES_MUESTRA_PREPROCESSED_PATH)
     print("Fin")
-
-
-preprocessing_dataset_es()
