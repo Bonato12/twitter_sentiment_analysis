6ed7544bd8761c8b1e3d2e4cd17c7498a362d18f
Bonato12 <sebabonato12@gmail.com>
2025-01-29 20:18:01 -0300

    Elimine constantes

--- config.py
+++ config.py
@@ -1,19 +1,7 @@
 import os
 
 PATH_BASE = os.path.dirname(os.path.abspath(__file__))
-DATASET_TRAIN_PATH_SPAM = PATH_BASE + "//datasets/spam.csv"
-DATASET_TRAIN_PATH = PATH_BASE + "//datasets/dataset_train.csv"
-DATASET_TRAIN_MUESTRA_PATH = PATH_BASE + "//datasets/dataset_train_muestra.csv"
-DATASET_TRAIN_MUESTRA_PREPROCESSED_PATH =  PATH_BASE + "//datasets/dataset_train_muestra_preprocessed.csv"
-DATASET_TRAIN_MUESTRA_PATH_TEST = PATH_BASE + "//datasets/dataset_train_muestra_test.csv"
 
-DATASET_PATH = PATH_BASE + "//datasets/dataset.csv"
 DATASET_ES_PATH = PATH_BASE + "//datasets/dataset_es.csv"
 DATASET_ES_MUESTRA_PATH = PATH_BASE + "//datasets/dataset_es_muestra.csv"
 DATASET_ES_MUESTRA_PREPROCESSED_PATH = PATH_BASE + "//datasets/dataset_es_muestra_preprocessed.csv"
-
-
-
-
-FILE_ID_DRIVE = '1n3nBaUf2HNdYrwA-w8td6E74enWta5VO'
-URL_DRIVE = f'https://drive.google.com/uc?id={FILE_ID_DRIVE}'
