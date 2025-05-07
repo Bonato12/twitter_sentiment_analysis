6a681381d09239aa678b98b4b386c45773cc548b
Bonato12 <sebabonato12@gmail.com>
2025-05-07 18:28:06 -0300

    cambios en los datasets

--- config.py
+++ config.py
@@ -6,9 +6,8 @@ DATASET_ES_PATH = PATH_BASE + "//datasets/dataset_es.csv"
 DATASET_ES_MUESTRA_PATH = PATH_BASE + "//datasets/dataset_es_muestra.csv"
 DATASET_ES_MUESTRA_PREPROCESSED_PATH = PATH_BASE + "//datasets/dataset_es_muestra_preprocessed.csv"
 
-DATASET_RESPONSE = PATH_BASE + "//respuestas12.csv"
-DATASET_RESPONSE_FINAL = PATH_BASE + "//respuestas122025.csv"
-DATASET_RESPONSE_MILEI_FINAL = PATH_BASE + "//response_milei.csv"
+DATASET_RESPONSE = PATH_BASE + "//datasets/response_f1.csv"
 
-DATASET_RESPONSE_PREPROCESSED_PATH = PATH_BASE + "//datasets/dataset_response_preprocessed.csv"
+
+
 
